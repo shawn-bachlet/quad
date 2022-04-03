493aa658e69b0f643666ed3a4ab90e673e621e65
sbachlet <sbachlet@gmail.com>
2022-04-03 19:23:59 -0400

    Successfully building

--- default.nix
+++ default.nix
@@ -2,10 +2,14 @@ let
   sources = import ./nix/sources.nix {};
   haskellNix = import sources.haskellNix {};
   pkgs = import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs;
-in pkgs.haskell-nix.project {
-  src = pkgs.haskell-nix.haskellLib.cleanGit {
-    name = "quad";
-    src = ./.;
-  };
-  compiler-nix-name = "ghc8107";
+  project = pkgs.haskell-nix.project {
+              src = pkgs.haskell-nix.haskellLib.cleanGit {
+                name = "quad";
+                src = ./.;
+              };
+              compiler-nix-name = "ghc8107";
+            };
+in {
+  exe = project.quad.components.exes.quad;
+  lib = project.quad.components.sublibs.quad-lib;
 }
--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,7 @@
 module MyLib (someFunc) where
 
+import RIO
+import RIO.ByteString.Lazy
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
