d262343d922bfe6653d102a13c3854d7657f377c
sbachlet <sbachlet@gmail.com>
2022-04-14 11:29:50 -0400

    Add docker

--- shell.nix
+++ shell.nix
@@ -1,6 +1,12 @@
-(import ./default.nix).project.shellFor {
+let
+  sources = import ./nix/sources.nix {};
+  nixpkgs = import sources.nixpkgs {};
+
+in (import ./default.nix).project.shellFor {
   withHoogle = false;
   tools = {
     cabal = "3.2.0.0";
+    ghcid = "0.8.7";
   };
+  buildInputs = [ nixpkgs.docker nixpkgs.git ];
 }
