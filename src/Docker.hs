cfd8ebbfc401bab60c9eab439cc81f37cd3ef2a3
sbachlet <sbachlet@gmail.com>
2022-04-14 11:53:00 -0400

    Add JSON parsing

--- src/Docker.hs
+++ src/Docker.hs
@@ -1,32 +1,43 @@
 module Docker where
 
+import Data.Aeson ((.:))
 import qualified Data.Aeson as Aeson
+import qualified Data.Aeson.Types as Aeson.Types
 import qualified Network.HTTP.Simple as HTTP
 import RIO
 import qualified Socket
 
-newtype Image = Image Text
+newtype Image = Image { imageToText :: Text }
   deriving (Eq, Show)
 
-newtype ContainerExitCode = ContainerExitCode Int
+newtype ContainerExitCode = ContainerExitCode { exitCodeToInt :: Int }
   deriving (Eq, Show)
-
-exitCodeToInt :: ContainerExitCode -> Int
-exitCodeToInt (ContainerExitCode code) = code
-
-imageToText :: Image -> Text
-imageToText (Image image) = image
 
 data CreateContainerOptions
   = CreateContainerOptions
       { image :: Image
       }
 
-createContainer :: CreateContainerOptions -> IO ()
+parseResponse
+    :: HTTP.Response ByteString
+    -> (Aeson.Value -> Aeson.Types.Parser a)
+    -> IO a
+parseResponse res parser = do
+  let result = do
+        value <- Aeson.eitherDecodeStrict (HTTP.getResponseBody res)
+        Aeson.Types.parseEither parser value
+  case result of
+    Left e -> throwString e
+    Right status -> pure status
+
+newtype ContainerId = ContainerId { containerIdToText :: Text }
+    deriving (Eq, Show)
+
+createContainer :: CreateContainerOptions -> IO ContainerId
 createContainer options = do
   manager <- Socket.newManager "/var/run/docker.sock"
   let image = imageToText options.image
-  let body =
+      body =
         Aeson.object
           [ ("Image", Aeson.toJSON image),
             ("Tty", Aeson.toJSON True),
@@ -34,12 +45,14 @@ createContainer options = do
             ("Cmd", "echo hello"),
             ("Entrypoint", Aeson.toJSON [Aeson.String "/bin/sh", "-c"])
           ]
-  let req =
+      parser = Aeson.withObject "create-container" $ \o -> do
+        cId <- o .: "Id"
+        pure $ ContainerId cId
+      req =
         HTTP.defaultRequest
           & HTTP.setRequestManager manager
           & HTTP.setRequestPath "/v1.40/containers/create"
           & HTTP.setRequestMethod "POST"
           & HTTP.setRequestBodyJSON body
   res <- HTTP.httpBS req
-  -- Dump the response to stdout to check what we're getting back.
-  traceShowIO res
+  parseResponse res parser
